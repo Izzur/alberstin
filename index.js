1339e3cad7ff5557f74b201197db999d9f6506f8
Izzur Zuhri <izzur.zuhri@gmail.com>
2018-04-20 15:45:51 +0700

    Add express.js to listen port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+express().listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
