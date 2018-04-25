1224767639ba762ee72890bf74d9e3a2c1012d61
Izzur Zuhri <izzur.zuhri@gmail.com>
2018-04-25 14:00:02 +0700

    Hotfix command for group

--- libs/meetup.js
+++ libs/meetup.js
@@ -4,22 +4,22 @@ const fetch = require('node-fetch');
 const api_host = 'https://api.meetup.com';
 var api_method = '/find/upcoming_events';
 var api_args = '?photo-host=public&page=15&order=time&topic_category=292';
-var api_key = '&key='+process.env.API_KEY_MEETUP;
+var api_key = '&key=' + process.env.API_KEY_MEETUP;
 
 exports = module.exports = main;
 
 function main(bot) {
-  bot.command('/meetup', ctx => meetup(ctx));
+  bot.command(['/meetup', 'meetup', '/meetup@AlberstinBot', 'meetup@AlberstinBot'], ctx => meetup(ctx));
 }
 
 function meetup(ctx) {
   let reply = '';
   fetch(api_host + api_method + api_args + api_key + '&sign=true')
     .then(res => res.json())
-    .then(function(meetups) {
+    .then(function (meetups) {
       for (let meetup of meetups.events) {
         reply += `[${meetup.local_date}] <a href="${meetup.link}">${meetup.name}</a>\n\n`;
       }
-      ctx.reply(reply, {'parse_mode': 'html'});
+      ctx.reply(reply, { 'parse_mode': 'html' });
     }).catch(err => console.log(err));
 }
