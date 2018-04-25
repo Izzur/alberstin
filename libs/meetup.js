d1f13d00a9c688a084abaf44de90601852fe10f8
Izzur Zuhri <izzur.zuhri@gmail.com>
2018-04-25 13:19:34 +0700

    Use API Key fot meetup.com, limit query, sort by date, topic tech only

--- libs/meetup.js
+++ libs/meetup.js
@@ -3,9 +3,8 @@
 const fetch = require('node-fetch');
 const api_host = 'https://api.meetup.com';
 var api_method = '/find/upcoming_events';
-var api_args = '?photo-host=public&page=20';
-var api_sig_id = '&sig_id=248974989';
-var api_sig = '&sig=291d274d6e242d5ae394b1df9165f3503724013d';
+var api_args = '?photo-host=public&page=15&order=time&topic_category=292';
+var api_key = '&key='+process.env.API_KEY_MEETUP;
 
 exports = module.exports = main;
 
@@ -15,7 +14,7 @@ function main(bot) {
 
 function meetup(ctx) {
   let reply = '';
-  fetch(api_host + api_method + api_args + api_sig_id + api_sig)
+  fetch(api_host + api_method + api_args + api_key + '&sign=true')
     .then(res => res.json())
     .then(function(meetups) {
       for (let meetup of meetups.events) {
